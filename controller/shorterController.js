b020b059d553857d864ebf20abe54d8482932f5a
aldiwira <aldiwira.aw@gmail.com>
2020-04-13 23:23:49 +0700

    fixing shorter function

--- controller/shorterController.js
+++ controller/shorterController.js
@@ -3,35 +3,9 @@ const response = require("../helper/response.js");
 let data, code, massage;
 module.exports = {
   getLink: async (req, res) => {
-    await linkModel
-      .find()
-      .then((datas) => {
-        code = response.CODE_SUCCESS;
-        massage = response.RESPONSE_SUCCESS;
-        data = datas;
-      })
-      .catch((err) => {
-        code = response.CODE_ERROR;
-        massage = response.RESPONSE_ERROR;
-        data = err;
-      });
-    res.status(code).json(response.set(code, massage, data));
-  },
-  createLinkData: async (req, res) => {
-    const valid = linkModel.find({
-      $or: [
-        { full_link: req.body.full_link },
-        { short_link: req.body.short_link },
-      ],
-    });
-    if ((await valid).length == 0) {
-      const link_data = new linkModel({
-        full_link: req.body.full_link,
-        short_link: req.body.short_link,
-        click_count: 0,
-      });
-      await link_data
-        .save()
+    try {
+      await linkModel
+        .find()
         .then((datas) => {
           code = response.CODE_SUCCESS;
           massage = response.RESPONSE_SUCCESS;
@@ -42,30 +16,74 @@ module.exports = {
           massage = response.RESPONSE_ERROR;
           data = err;
         });
-    } else {
-      code = response.CODE_UNAUTHORIZED;
+    } catch (error) {
+      code = response.CODE_ERROR;
       massage = response.RESPONSE_ERROR;
-      data = "Your link has shorter link";
+      data = error;
+    }
+    res.status(code).json(response.set(code, massage, data));
+  },
+  createLinkData: async (req, res) => {
+    const link_data = new linkModel({
+      full_link: req.body.full_link,
+      short_link: req.body.short_link,
+      click_count: 0,
+    });
+    try {
+      const valid = linkModel.find({
+        $or: [
+          { full_link: req.body.full_link },
+          { short_link: req.body.short_link },
+        ],
+      });
+      if ((await valid).length == 0) {
+        await link_data
+          .save()
+          .then((datas) => {
+            code = response.CODE_CREATED;
+            massage = response.RESPONSE_CREATED;
+            data = datas;
+          })
+          .catch((err) => {
+            code = response.CODE_ERROR;
+            massage = response.RESPONSE_ERROR;
+            data = err;
+          });
+      } else {
+        code = response.CODE_UNAUTHORIZED;
+        massage = response.RESPONSE_ERROR;
+        data = "Your link has shorter link";
+      }
+    } catch (error) {
+      code = response.CODE_ERROR;
+      massage = response.RESPONSE_ERROR;
+      data = error;
     }
     res.status(code).json(response.set(code, massage, data));
   },
   deleteLinkData: async (req, res) => {
     let short_link = req.query.short_link;
     let full_link = req.query.full_link;
-    linkModel
-      .findOneAndDelete({
-        $or: [{ short_link: short_link }, { full_link: full_link }],
-      })
-      .then((datas) => {
-        code = response.CODE_SUCCESS;
-        massage = "Link has deleted";
-        data = [];
-      })
-      .catch((err) => {
-        code = response.CODE_ERROR;
-        massage = "your link is unknown";
-        data = err;
-      });
+    try {
+      linkModel
+        .findOneAndDelete({
+          $or: [{ short_link: short_link }, { full_link: full_link }],
+        })
+        .then((datas) => {
+          code = response.CODE_SUCCESS;
+          massage = "Link has deleted";
+          data = [];
+        })
+        .catch((err) => {
+          code = response.CODE_ERROR;
+          massage = "your link is unknown";
+          data = err;
+        });
+    } catch (error) {
+      code = response.CODE_ERROR;
+      massage = "your link is unknown";
+      data = err;
+    }
     res.status(code).json(response.set(code, massage, data));
   },
 };

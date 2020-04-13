ad4f2380de9373700730fcb519f0639cbe90b3cb
aldiwira <aldiwira.aw@gmail.com>
2020-04-13 23:40:59 +0700

    add status response created

--- helper/response.js
+++ helper/response.js
@@ -2,6 +2,8 @@ module.exports = {
   CODE_SUCCESS: 200,
   CODE_ERROR: 401,
   CODE_UNAUTHORIZED: 403,
+  CODE_CREATED: 201,
+  RESPONSE_CREATED: "Data created",
   RESPONSE_SUCCESS: "Data Loaded",
   RESPONSE_ERROR: "Error",
   RESPONSE_WRONG: "Data Error",

eb00849e9dcbc404262c6d715413468cc2931806
aldiwira <aldiwira.aw@gmail.com>
2020-04-13 23:26:17 +0700

    re create db connection

--- config/db.js
+++ config/db.js
@@ -4,12 +4,6 @@ const dotenv = require("dotenv/config");
 Mongoose.connect(process.env.DB_HOST, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log("Connection Success");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+});
 
 module.exports = Mongoose;

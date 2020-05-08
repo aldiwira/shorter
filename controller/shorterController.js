81f7541f8a7a6e09dec8291295b1e199ac687412
aldiwira <aldiwira.aw@gmail.com>
2020-05-08 16:03:31 +0700

    edit create link data

--- controller/shorterController.js
+++ controller/shorterController.js
@@ -1,6 +1,18 @@
 const linkModel = require("../models/linkModel.js");
 const response = require("../helper/response.js");
 let data, code, massage;
+const checkingLinkDatas = async (datas) => {
+  return await linkModel.findOne({
+    $or: [
+      {
+        full_link: datas.full_link,
+      },
+      {
+        short_link: datas.short_link,
+      },
+    ],
+  });
+};
 module.exports = {
   getLink: async (req, res) => {
     try {
@@ -24,40 +36,28 @@ module.exports = {
     res.status(code).json(response.set(code, massage, data));
   },
   createLinkData: async (req, res) => {
-    const link_data = new linkModel({
+    const datas = {
       full_link: req.body.full_link,
       short_link: req.body.short_link,
       click_count: 0,
-    });
-    try {
-      const valid = linkModel.find({
-        $or: [
-          { full_link: req.body.full_link },
-          { short_link: req.body.short_link },
-        ],
-      });
-      if ((await valid).length == 0) {
-        await link_data
-          .save()
-          .then((datas) => {
-            code = response.CODE_CREATED;
-            massage = response.RESPONSE_CREATED;
-            data = datas;
-          })
-          .catch((err) => {
-            code = response.CODE_ERROR;
-            massage = response.RESPONSE_ERROR;
-            data = err;
-          });
-      } else {
-        code = response.CODE_UNAUTHORIZED;
-        massage = response.RESPONSE_ERROR;
-        data = "Your link has shorter link";
-      }
-    } catch (error) {
+    };
+    if ((await checkingLinkDatas(datas)) === null) {
+      await linkModel
+        .create(datas)
+        .then((result) => {
+          status = response.CODE_CREATED;
+          massage = response.RESPONSE_CREATED;
+          data = result;
+        })
+        .catch((err) => {
+          code = response.CODE_ERROR;
+          massage = response.RESPONSE_ERROR;
+          data = err;
+        });
+    } else {
       code = response.CODE_ERROR;
       massage = response.RESPONSE_ERROR;
-      data = error;
+      data = "Your link has shorter link";
     }
     res.status(code).json(response.set(code, massage, data));
   },

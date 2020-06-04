92606b1baa0439234adf1f93a47382003fc5d68c
aldiwira <aldiwira.aw@gmail.com>
2020-06-04 16:12:17 +0700

    Fixing all mongo models timestamps

--- models/linkModel.js
+++ models/linkModel.js
@@ -1,33 +1,33 @@
 const Mongoose = require("mongoose");
 
-const link = Mongoose.Schema({
-  full_link: {
-    type: String,
-    required: true,
+const link = Mongoose.Schema(
+  {
+    full_link: {
+      type: String,
+      required: true,
+    },
+    short_link: {
+      type: String,
+      require: true,
+    },
+    click_count: {
+      type: Number,
+      require: true,
+    },
+    owner: {
+      type: String,
+      require: true,
+    },
+    lastVisit: {
+      type: Date,
+    },
   },
-  short_link: {
-    type: String,
-    require: true,
-  },
-  click_count: {
-    type: Number,
-    require: true,
-  },
-  owner: {
-    type: String,
-    require: true,
-  },
-  lastVisit: {
-    type: Date,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
 
 module.exports = Mongoose.model("link", link);
--- models/userModel.js
+++ models/userModel.js
@@ -1,34 +1,32 @@
 const Mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-
-const users = Mongoose.Schema({
-  email: {
-    type: String,
-    trim: true,
-    required: true,
+const users = Mongoose.Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    username: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    _isLogin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  _isLogin: {
-    type: Boolean,
-    default: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
 
 module.exports = Mongoose.model("users", users);
